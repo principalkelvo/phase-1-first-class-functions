a5136b0c13738ac4a52569254b886f3ab71e6161
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-08 15:39:58 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback){
+  callback();
+}
+
+function returnsANamedFunction(){
+  const fn = function(){};
+  return fn;
+}
+function returnsAnAnonymousFunction(){
+  return function(){};
+
+}
+
